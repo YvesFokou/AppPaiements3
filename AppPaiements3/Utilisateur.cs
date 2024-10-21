13668f13284a71bbf7f4397e77329905e2a1b852
Yves Fokou <300138329@monboreal.ca>
2024-10-21 08:15:11 -0700

    Ajout de L'utilisateur

---
+++ AppPaiements3/Utilisateur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements3
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> liste_paiements { get; set; }
+
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            liste_paiements = new List<Paiement>();
+        }
+
+        public void AjouterPaiement(Paiement paiement)
+        {
+            liste_paiements.Add(paiement);
+        }
+
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Utilisateur: {Nom}");
+            Console.WriteLine("Liste des paiements:");
+            foreach (var paiement in liste_paiements)
+            {
+                paiement.AfficherDetails();
+            }
+        }
+    }
+}
