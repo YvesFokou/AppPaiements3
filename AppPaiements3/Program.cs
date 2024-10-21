89d2f763469f8a75a12c88c06ac2a5ff36bae25e
Yves Fokou <300138329@monboreal.ca>
2024-10-21 08:00:08 -0700

    Ajout de la classe CarteCredit et des instances

--- AppPaiements3/Program.cs
+++ AppPaiements3/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using AppPaiements3;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        CarteCredit paiement1 = new CarteCredit(344.00, "Achat telephone", 46367);
+        CarteCredit paiement2 = new CarteCredit(132.00, "Achat ordinateur",7667661);
+        CarteCredit paiement3 = new CarteCredit(985.00, "Achat Vetements",88887);
+
+        paiement1.AfficherDetails();
+        paiement2.AfficherDetails();
+        paiement3.AfficherDetails();
     }
 }
