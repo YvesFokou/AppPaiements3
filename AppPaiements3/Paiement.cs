82ac820c0713f95a98d11bcd952268672a36c8c7
Yves Fokou <300138329@monboreal.ca>
2024-10-21 07:52:16 -0700

    Ajout de la classe abstraite paiement

---
+++ AppPaiements3/Paiement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements3
+{
+    internal class Paiement
+    {
+
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+
+
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+
+        }
+
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Montant :{Montant},Description:{Description}");
+        }
+    }
+}
