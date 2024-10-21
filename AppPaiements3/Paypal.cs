c5b5f584df810d97f28e07b142937a0578350009
Yves Fokou <300138329@monboreal.ca>
2024-10-21 08:09:41 -0700

    Ajout de la classe paypal

---
+++ AppPaiements3/Paypal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements3
+{
+    internal class Paypal : Paiement
+    {
+        public string Courriel { get; set; }
+
+        public Paypal(double montant, string description, string courriel) : base(montant, description)
+        {
+            Courriel = courriel;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"PayPal - Montant: {Montant}, Description: {Description}, Courriel: {Courriel}");
+        }
+    }
+}
--- AppPaiements3/Program.cs
+++ AppPaiements3/Program.cs
@@ -12,5 +12,16 @@ internal class Program
         paiement1.AfficherDetails();
         paiement2.AfficherDetails();
         paiement3.AfficherDetails();
+
+
+
+        Paypal paiementPaypal1 = new Paypal(346.00, "Achat en ligne", "yves@exemple.ca");
+        Paypal paiementPaypal2 = new Paypal(538.00, "Achat en ligne", "ursanne@exemple.ca");
+
+        paiementPaypal1.AfficherDetails();
+        paiementPaypal2.AfficherDetails();
+
     }
+
+
 }
